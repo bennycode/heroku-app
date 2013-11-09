034a01586949722047afbc7a761aa511834a9060
Benny Neugebauer <bn@bennyn.de>
2013-11-09 17:53:05 +0100

    grails install-templates

---
+++ grails-app/controllers/heroku/app/EntryController.groovy
@@ -0,0 +1,5 @@
+package heroku.app
+
+class EntryController {
+    def scaffold = Entry
+}
---
+++ grails-app/domain/heroku/app/Entry.groovy
@@ -0,0 +1,15 @@
+package heroku.app
+
+class Entry {
+  static constraints = {
+    title()
+    summary(maxSize:1000)
+    dateCreated()
+    lastUpdated()
+  }
+
+  String title
+  String summary
+  Date dateCreated
+  Date lastUpdated
+}
